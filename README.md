2663e3cb120151643baec581c4ff145f0f831712
Kelly Lee Ying Ying <kellylee_90@live.com>
2023-06-18 16:02:10 +0800

    completed the web application

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # may23-team06
 
+This project is the submission for Heicoders AI300 course.
+
 Team Number : 06
+
 Members: Joshua W. & Kelly L.
 
 Model: XGBoost Classifier
 
 Parameters: 'colsample_bytree': 0.4, 'learning_rate': 0.02, 'max_depth': 3, 'min_child_weight': 12, 'n_estimators': 1000
+
 Using GridSearchCV
 
 AUC Score: 0.87
--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from model import Model
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,19 +11,29 @@ def home():
     if request.method == 'POST':
         form_input = dict(request.form)
 
-        # # Modified code
-        # age = int(form_input['age'])
-        # sex = form_input['sex']
-        # bmi = float(form_input['bmi'])
-        # children = int(form_input['children'])
-        # smoker = form_input['smoker']
-        # #region = form_input['region']
+        # inputs from user
+        gender = int(form_input['gender'])
+        age = int(form_input['age'])
+        senior_citizen = int(form_input['senior_citizen'])
+        married = int(form_input['married'])
+        num_dependents = int(form_input['num_dependents'])
+        num_referrals = int(form_input['num_referrals'])
+        avg_gb_download_monthly = float(form_input['avg_gb_download_monthly'])
+        total_monthly_fee = float(form_input['total_monthly_fee'])
+        total_charges_quarter = float(form_input['total_charges_quarter'])
 
-        # model_inputs = [age, sex, bmi, children, smoker, region]
-        # prediction = Model().predict(model_inputs)
+        # preparing inputs for model
+        model_inputs = [[gender, age, senior_citizen, married, num_dependents, num_referrals, avg_gb_download_monthly,
+                          total_monthly_fee, total_charges_quarter]]
+        column = ['gender', 'age', 'senior_citizen', 'married', 'num_dependents', 'num_referrals', 'avg_gb_download_monthly',
+                          'total_monthly_fee', 'total_charges_quarter']
+        input = pd.DataFrame(model_inputs,columns=column)
 
-        #return render_template('index.html', prediction=prediction) # Add parameter here
-    return render_template('index.html')
-
+        # make prediction
+        prediction = Model().predict(input)
+        print(prediction)
+        return render_template('index.html', prediction=prediction) # Add parameter here
+    else:
+        print("notig")
 if __name__ == '__main__':
     app.run(debug=True)
