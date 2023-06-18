2fe7e9fdf9cdc53e484bf5d2b24a7b85e84a9f02
Kelly Lee Ying Ying <kellylee_90@live.com>
2023-06-18 12:41:23 +0800

    added working model file

---
+++ src/model.py
@@ -0,0 +1,26 @@
+import joblib
+import pandas as pd
+
+class Model:
+    def __init__(self):
+        self.model = joblib.load('model/xgboost_model.pkl')
+
+    def predict(self, input_features):
+        return self.model.predict(input_features)
+    
+feature = {
+    'gender' : 0,
+    'age' : 38,
+    'senior_citizen' : 0,
+    'married' : 0,
+    'num_dependents' : 1,
+    'num_referrals' : 1,
+    'avg_gb_download_monthly' : 12,
+    'total_monthly_fee' : 50.2,
+    'total_charges_quarter' : 300.2
+}
+
+model_inputs = pd.DataFrame([feature])
+
+print(model_inputs)
+print(Model().predict(model_inputs))
